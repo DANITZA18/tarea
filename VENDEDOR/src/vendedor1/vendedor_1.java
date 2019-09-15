54f3f5b6e2d83c4417edafc9404ec6ba3f21c010
danitza18 <danitzamarlenechurasocujayo@gmail.com>
2019-09-15 02:27:14 -0500

    terminado pero con dos errores de libreria

---
+++ VENDEDOR/src/vendedor1/vendedor_1.java
@@ -0,0 +1,13 @@
+package vendedor1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+	@SpringBootApplication
+	public class  vendedor_1 { 
+
+	    public static void main(String... args) {
+	        SpringApplication.run(vendedor_1.class, args);
+	    }
+	
+}
